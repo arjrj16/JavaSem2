b762f054aea94978cec7ab8feda79e7ade0c0bee
arjrj16 <arjunmelwani@gmail.com>
2021-02-26 12:02:10 -0600

    Hw Finished

--- pg395ex4.java
+++ pg395ex4.java
@@ -1,5 +1,15 @@
-import java.util.Random;
+import java.util.*;
 public class pg395ex4 {
+    public static void gcd(int num1, int num2){
+        int gcd = 1;
+        for(int i = 1; i <= num1 && i <= num2; i++) {
+            if(num1%i==0 && num2%i==0) {
+                gcd = i;
+            }
+        }
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + gcd);
+    }
+    
     public static void randomX(){
         Random randy = new Random();
         int x = randy.nextInt(15)+5;
@@ -46,10 +56,44 @@ public class pg395ex4 {
         }
         System.out.println("It took "+loops+" moves and the max position was "+ max);
     }
+    public static void hopScotch(int HOPS){
+        int current = 1;
+        int total = 3 * HOPS+ 1;
+        while(current < total){
+            if((current - 1) % 3 == 0){
+                System.out.println("   " + current);
+                current++;
+            }
+            else{
+                System.out.println(current + "     " + (current + 1));
+                current += 2;
+            }
+            System.out.println("   " + current);
+        }
+    }
+    public static void printAverage(){
+        Scanner in = new Scanner (System.in);
+        int uNum=0;
+        int loop =0;
+        int sum =0;
+        while(uNum>-1){
+            System.out.println("Type a number:");
+            uNum = in.nextInt();
+            if(uNum>-1){
+            sum = sum + uNum;
+            loop++;
+            }
+        }
+        System.out.println("The average is "+((double)sum/loop));
+
+    }
     public static void main (String args []){
         
         //randomX();
        // makeGuesses();
-        randomWalk();
+       // randomWalk();
+      // gcd(24,6);
+       //hopScotch(3);
+       printAverage();
     }
 }
